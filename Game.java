7fc91844aa57478bc051462eca78840e30a72f34
dayyad <ease.spam@gmail.com>
2016-08-09 01:35:17 +1200

    Started work on server

--- Game.java
+++ Game.java
@@ -1,9 +1,29 @@
+import ecs100.*;
+import java.io.*;
+import java.net.*;
 
 public class Game {
+	
+	public Game (){
+		UI.initialise();
+		UI.clearGraphics();
+		UI.setImmediateRepaint(false);
+		
+		UI.addButton("Connect to Server", this::connectServer);
+		UI.addButton("Create Server", this::createServer);
+	}
+	
+	private void connectServer(){
+		
+	}
+	
+	private void createServer(){
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//testing
+		
 	}
 
 }
--- Server.java
+++ Server.java
@@ -1,4 +1,28 @@
+import ecs100.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class Server {
-
+	private int port;
+	
+	
+	public Server (){
+		port = UI.askInt("Enter Port:");
+		run();
+	}
+	
+	private void run()  {
+		try{
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket socket = serverSocket.accept();
+			Scanner scanner = new Scanner(socket.getInputStream());
+			
+			
+			
+			
+		} catch (IOException e){
+			UI.println("Exception: " + e);
+		}
+	}
 }
